c2059a673f9e3bc9fde2670cf82d14d19c2c18db
Ruslan <ruslan.arifullin@gmail.com>
2017-07-23 02:09:43 +0300

    master - revert corrupted version

--- src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
@@ -1,19 +1,16 @@
 package ru.javawebinar.topjava.repository;
 
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.to.MealWithExceed;
 
-import java.time.LocalTime;
 import java.util.Collection;
 
 public interface MealRepository {
-    Meal save(Meal meal, int userId);
+    Meal save(Meal meal);
 
-    boolean delete(int id, int userId);
+    void delete(int id);
 
-    Meal get(int id, int userId);
+    Meal get(int id);
 
-    Collection<Meal> getAll(int userId);
+    Collection<Meal> getAll();
 
-    Collection<Meal> getBetween(LocalTime startDT, LocalDateTime endDT, int userId);
 }
