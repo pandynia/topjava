af3a1baa329b8b393c7f3c3c4266f58bb48e3ef9
Ruslan <ruslan.arifullin@gmail.com>
2017-07-23 02:10:37 +0300

    master - revert corrupted version

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,9 +1,8 @@
 package ru.javawebinar.topjava.web.meal;
 
-import org.springframework.stereotype.Controller;
 import ru.javawebinar.topjava.service.MealService;
 
-@Controller
+
 public class MealRestController {
     private MealService service;
 
