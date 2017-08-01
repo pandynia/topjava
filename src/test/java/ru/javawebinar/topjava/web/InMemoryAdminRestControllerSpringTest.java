a3a2b14f71b7f868896ff4c7c22f4e0f305da321
Ruslan <ruslan.arifullin@gmail.com>
2017-08-02 00:48:13 +0300

    HW3_optional - ImMemoryAdminRestControllerSpringTest is copied from master

--- src/test/java/ru/javawebinar/topjava/web/InMemoryAdminRestControllerSpringTest.java
+++ src/test/java/ru/javawebinar/topjava/web/InMemoryAdminRestControllerSpringTest.java
@@ -6,23 +6,27 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.repository.mock.InMemoryUserRepositoryImpl;
+import ru.javawebinar.topjava.repository.UserRepository;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
+import ru.javawebinar.topjava.web.user.AdminRestController;
 
 import java.util.Collection;
 
 import static ru.javawebinar.topjava.UserTestData.ADMIN;
 import static ru.javawebinar.topjava.UserTestData.USER;
 
-@ContextConfiguration({"classpath:spring/spring-test.xml", "classpath:spring/spring-db.xml"})
-@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring/spring-app.xml")
+@RunWith(SpringRunner.class)
 public class InMemoryAdminRestControllerSpringTest {
 
     @Autowired
-    private InMemoryUserRepositoryImpl repository;
+    private AdminRestController controller;
+
+    @Autowired
+    private UserRepository repository;
 
     @Before
     public void setUp() throws Exception {
@@ -33,14 +37,14 @@ public class InMemoryAdminRestControllerSpringTest {
 
     @Test
     public void testDelete() throws Exception {
-        repository.delete(UserTestData.USER_ID);
-        Collection<User> users = repository.getAll();
+        controller.delete(UserTestData.USER_ID);
+        Collection<User> users = controller.getAll();
         Assert.assertEquals(users.size(), 1);
         Assert.assertEquals(users.iterator().next(), ADMIN);
     }
 
     @Test(expected = NotFoundException.class)
     public void testDeleteNotFound() throws Exception {
-        repository.delete(10);
+        controller.delete(10);
     }
 }
