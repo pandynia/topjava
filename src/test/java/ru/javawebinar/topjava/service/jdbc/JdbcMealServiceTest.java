2cc02eac718373db9da1c5ad2f776cbeb9d9b0f3
Ruslan <ruslan.arifullin@gmail.com>
2017-08-30 00:49:55 +0300

    HW06 - Optonal: small changes

--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
@@ -2,10 +2,11 @@ package ru.javawebinar.topjava.service.jdbc;
 
 import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
-import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.service.AbstractMealServiceTest;
 
-@ActiveProfiles({Profiles.JDBC, Profiles.POSTGRES_DB})
+import static ru.javawebinar.topjava.Profiles.JDBC;
+
+@ActiveProfiles(JDBC)
 public class JdbcMealServiceTest extends AbstractMealServiceTest {
     @Override
     @Test
