fbb1f42bdb5c4eb84835561b63ec16182108ee40
Ruslan <ruslan.arifullin@gmail.com>
2017-08-31 00:43:33 +0300

    HW06 - Optonal: fixes are done

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepositoryImpl.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
@@ -16,6 +17,8 @@ import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
 import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.*;
 
 @Repository
@@ -48,12 +51,13 @@ public class JdbcUserRepositoryImpl implements UserRepository {
         if (user.isNew()) {
             Number newKey = insertUser.executeAndReturnKey(parameterSource);
             user.setId(newKey.intValue());
+            batchUpdate(user);
         } else {
-            deleteRoles(user);
-            insertRoles(user);
             namedParameterJdbcTemplate.update(
                     "UPDATE users SET name=:name, email=:email, password=:password, " +
                             "registered=:registered, enabled=:enabled, calories_per_day=:caloriesPerDay WHERE id=:id", parameterSource);
+            jdbcTemplate.update("DELETE FROM user_roles WHERE USER_ID=?", user.getId());
+            batchUpdate(user);
         }
         return user;
     }
@@ -79,13 +83,15 @@ public class JdbcUserRepositoryImpl implements UserRepository {
 
     @Override
     public List<User> getAll() {
-        SqlRowSet rowSet = jdbcTemplate.queryForRowSet("SELECT * FROM users");
-        Map<Integer, Set<Role>> map = new HashMap<>();
-        while (rowSet.next()) {
-            Set<Role> roles = map.computeIfAbsent(rowSet.getInt("user_id"), userId -> EnumSet.noneOf(Role.class));
-            roles.add(Role.valueOf(rowSet.getString("role")));
-        }
         List<User> users = jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
+        Map<Integer, Set<Role>> map = new HashMap<>();
+        SqlRowSet rowSet = jdbcTemplate.queryForRowSet("SELECT * FROM users");
+
+        while (rowSet.next()) {
+            map.computeIfAbsent(rowSet.getInt("user_id"), userId -> EnumSet.noneOf(Role.class));
+            map.get(rowSet.getInt("user_id")).add(Role.valueOf(rowSet.getString("role")));
+        }
+
         users.forEach(u -> u.setRoles(map.get(u.getId())));
         return users;
     }
@@ -112,5 +118,25 @@ public class JdbcUserRepositoryImpl implements UserRepository {
         }
 
         return u;
+    }
+
+    protected void batchUpdate(User user) {
+        if (!CollectionUtils.isEmpty(user.getRoles())) {
+            jdbcTemplate.batchUpdate("INSERT INTO user_roles(iser_id, roles) VALUES (?, ?)", new BatchPreparedStatementSetter() {
+                List<Role> roles = new ArrayList<Role>(user.getRoles());
+
+                @Override
+                public void setValues(PreparedStatement preparedStatement, int i) throws SQLException {
+                    Role role = roles.get(i);
+                    preparedStatement.setInt(1, user.getId());
+                    preparedStatement.setString(2, role.toString());
+                }
+
+                @Override
+                public int getBatchSize() {
+                    return roles.size();
+                }
+            });
+        }
     }
 }
