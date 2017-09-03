34af7d4d1009a5e914be9cceef81624a3b4a7c61
Ruslan <ruslan.arifullin@gmail.com>
2017-09-04 01:25:58 +0300

    HW07 - Task 2 20% is ready

---
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
@@ -0,0 +1,37 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import ru.javawebinar.topjava.AuthorizedUser;
+import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.AbstractControllerTest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.javawebinar.topjava.MealTestData.MATCHER_WITH_EXCEED;
+import static ru.javawebinar.topjava.MealTestData.MEALS;
+
+
+/**
+ * Created by Ruslan on 04.09.2017.
+ */
+public class MealRestControllerTest extends AbstractControllerTest {
+    static final String REST_URL= MealRestController.REST_URL+'/';
+
+    @Autowired
+    private MealService mealService;
+
+    @Test
+    public void testGetAll() throws Exception {
+        mockMvc.perform(get(REST_URL))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(MATCHER_WITH_EXCEED.contentListMatcher(MealsUtil.getWithExceeded(MEALS, AuthorizedUser.getCaloriesPerDay())));
+
+    }
+
+
+}
