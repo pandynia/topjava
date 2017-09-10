3352d87302ef5acf445abb83f2fbd20ffb5a731d
Ruslan <ruslan.arifullin@gmail.com>
2017-09-11 00:51:34 +0300

    HW08 - fixed files

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -12,56 +12,51 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
-
 @RestController
-@RequestMapping(MealRestController.REST_URL)
+@RequestMapping(value = MealRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 public class MealRestController extends AbstractMealController {
-
     static final String REST_URL = "/rest/profile/meals";
 
     @Override
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping("/{id}")
+    public Meal get(@PathVariable("id") int id) {
+        return super.get(id);
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") int id) {
+        super.delete(id);
+    }
+
+    @Override
+    @GetMapping
     public List<MealWithExceed> getAll() {
         return super.getAll();
     }
 
     @Override
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Meal get(@PathVariable("id") int id) {
-        return super.get(id);
-    }
-
-    @DeleteMapping(value = "/{id}")
-    public void delete(@PathVariable("id") int id) {
-        super.delete(id);
-    }
-
     @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void update(@RequestBody Meal meal, @PathVariable("id") int id) {
-        super.update(meal,id);
+        super.update(meal, id);
     }
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Meal> createWithLocation(@RequestBody Meal meal) {
-        Meal created=super.create(meal);
+        Meal created = super.create(meal);
+
         URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
                 .path(REST_URL + "/{id}")
                 .buildAndExpand(created.getId()).toUri();
+
         return ResponseEntity.created(uriOfNewResource).body(created);
     }
 
-    @GetMapping(value = "/filter", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Override
+    @GetMapping(value = "/filter")
     public List<MealWithExceed> getBetween(
-            @RequestParam("startDate")
-                    LocalDate startDate,
-            @RequestParam("endDate")
-                    LocalDate endDate,
-            @RequestParam("startTime")
-                    LocalTime startTime,
-            @RequestParam("endTime")
-                    LocalTime endTime)
-    {
+            @RequestParam(value = "startDate", required = false) LocalDate startDate, @RequestParam(value = "startTime", required = false) LocalTime startTime,
+            @RequestParam(value = "endDate", required = false) LocalDate endDate, @RequestParam(value = "endTime", required = false) LocalTime endTime) {
         return super.getBetween(startDate, startTime, endDate, endTime);
     }
-
 }
