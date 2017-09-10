fa9c74f3364c9ec117c2b70b652beb1ac9c319a6
Ruslan <ruslan.arifullin@gmail.com>
2017-09-11 01:03:08 +0300

    HW08 - fix files

--- src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
@@ -12,12 +12,12 @@ import java.time.LocalTime;
 import java.util.List;
 
 /**
- * Created by Ruslan on 10.09.2017.
+ * Created by User on 009 09.09.17.
  */
 @RestController
-@RequestMapping(MealAjaxController.REST_URL)
+@RequestMapping(value = MealAjaxController.REST_URL)
 public class MealAjaxController extends AbstractMealController {
-    static final String REST_URL = "/rest/profile/meals";
+    static final String REST_URL = "ajax/profile/meals";
 
     @Override
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
@@ -25,32 +25,31 @@ public class MealAjaxController extends AbstractMealController {
         return super.getAll();
     }
 
-    @DeleteMapping(value = "/{id}")
+    @Override
+    @DeleteMapping("/{id}")
     public void delete(@PathVariable("id") int id) {
         super.delete(id);
     }
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void createWithLocation(@RequestParam("id") Integer id,
-                                   @RequestParam("dateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateTime,
-                                   @RequestParam("description") String description,
-                                   @RequestParam("calories") Integer calories
-                                   ) {
-        Meal meal= new Meal(id, dateTime, description, calories);
+    @PostMapping
+    public void createOrUpdate(@RequestParam("id") Integer id,
+                               @RequestParam("description") String description,
+                               @RequestParam("calories") Integer calories,
+                               @RequestParam("dateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateTime) {
+        Meal meal = new Meal(id,dateTime,
+                description,
+                calories);
+
         if (meal.isNew()) {
             super.create(meal);
         }
     }
 
-    @GetMapping(value = "/filter", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<MealWithExceed> getBetween(
-            @RequestParam(value = "startDate", required = false) LocalDate startDate,
-            @RequestParam(value = "endDate", required = false) LocalDate endDate,
-            @RequestParam(value = "startTime", required = false) LocalTime startTime,
-            @RequestParam(value = "endTime", required = false) LocalTime endTime)
-    {
+    @GetMapping(value="/filter", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<MealWithExceed> getFiltered(@RequestParam(value = "startDate", required = false) LocalDate startDate,
+                                            @RequestParam(value = "endDate", required = false) LocalDate endDate,
+                                            @RequestParam(value = "startTime", required = false) LocalTime startTime,
+                                            @RequestParam(value = "endTime", required = false) LocalTime endTime) {
         return super.getBetween(startDate, startTime, endDate, endTime);
     }
-
-
 }
